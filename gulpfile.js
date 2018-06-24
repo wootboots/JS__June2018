ee5fdf6ed6f0212b68f88f863a92d5a8613ee3fb
wootboots <jimroache@gmail.com>
2018-06-24 16:45:44 -0400

    Postcss runners

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'), 
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 gulp.task('default', function(){
     console.log("Hurrah!");
@@ -9,8 +13,18 @@ gulp.task('html', function() {
     console.log("Imagine...");
 });
 
+gulp.task('styles', function() {
+    return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
 gulp.task('watch', function(){
     watch('./app/index.html', function(){
         gulp.start('html');
+    });
+
+    watch('./app/assets/styles/**/*.css', function(){
+        gulp.start('styles');
     });
 })
